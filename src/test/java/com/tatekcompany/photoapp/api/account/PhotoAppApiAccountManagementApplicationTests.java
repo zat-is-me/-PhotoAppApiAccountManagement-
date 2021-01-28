b38ccd3b21ad4cad54e9819e7fee6a39db09a38a
zat-is-me <taahm1@morgan.edu>
2021-01-27 20:49:24 -0600

    Users Management API

---
+++ src/test/java/com/tatekcompany/photoapp/api/account/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatekcompany.photoapp.api.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
